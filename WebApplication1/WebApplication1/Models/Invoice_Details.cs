4d53927a1a6f6eafaf2433a3110b3a168c4792af
JanithSandaru <janithsandaru999@gmail.com>
2022-09-26 23:28:18 +0530

    updated

---
+++ WebApplication1/WebApplication1/Models/Invoice_Details.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApplication1.Models
+{
+    public class Product
+    {
+        public int Products_id { get; set; }
+        public string Product_Name { get; set; }
+        public string Products_price { get; set; }
+        public int Products_qty { get; set; }
+    }
+}
